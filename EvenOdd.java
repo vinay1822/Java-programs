3a221e23c15f5c74629477e6ac6912f6af0ab08c
Vinay Hiragond <hiragondvinay@gmail.com>
2023-03-22 09:18:43 +0530

    EvenOdd program added

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+// program go print whether the number is ever of odd
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        if(num % 2 == 0) {
+            System.out.println(num + " is even");
+        }else {
+            System.out.println(num + " is odd");
+        }
+
+    }
+}
